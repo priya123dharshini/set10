a65f3f51091ff3e0ac3dffff4529dd24e856e444
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-09 14:32:09 +0530

    Create numk.c

---
+++ numk.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n,k,i;
+printf("enter n value and k value");
+scanf("%d %d",&n,&k);
+if(n>0)
+{
+  for(i=1;i<n;i++)
+  {
+   printf("%d",i);
+   }
+ }
+ printf("\n%d",&k);
+ }
