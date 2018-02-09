605eef6a5c5b0dbb3ad4df54629d1c5b917ae0a1
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-02-09 14:18:35 +0530

    Create sumofn.c

---
+++ sumofn.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+ int n,a,sum=0;
+ printf("enter the amount of numbers want to add");
+ scanf("%d",&n);
+ printf("enter the numbers");
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",&a);
+      sum=sum+a;
+    }
+  }
+  }
+  
