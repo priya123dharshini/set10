41959aecac7951a19abca2d37a0a9400cfab1f9f
priya123dharshini <30829459+priya123dharshini@users.noreply.github.com>
2018-03-02 16:21:37 +0530

    Update ascen.c

--- ascen.c
+++ ascen.c
@@ -1,11 +1,27 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+int main()
 {
- int n[10];
- printf("enter numbers");
- for(i=0;i<10;i++)
- {
- scanf("%d",n);
+int arr max_n;
+    int i, max, min, n;
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+    printf("Enter elements in the array: ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
  }
- n[0]=smallest;
- fo
+    
+    for(i=1; i<n; i++)
+    {
+
+        min==arr[0];
+  
+        if(arr[i] > min)
+        {
+           
+ 
+    printf("Maximum element = %d\n", i);
+    
+
+    return 0;
+}
